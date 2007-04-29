5d587b0dccf64da80569e5211757a2d83e0a8597
Michael Dickey <mike@atomiclabs.com>
2007-04-29 22:05:45 +0000

    Added more ignore rules for subversion

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,10 @@
 # DO NOT USE autoheader -> config.h.in file is NOT automanaged!!!
 #autoheader
 
+# Note: this will produce warnings after it has already been run
+# but they can be safely ignored
+libtoolize
+
 aclocal -I build
 autoconf
 automake -a
