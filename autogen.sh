5cb198c79386745fa13e0bc552c5570c9c97137b
Michael Dickey <mike@atomiclabs.com>
2008-03-16 18:52:52 +0000

    http://trac.atomiclabs.com/ticket/14
    Made PionScheduler more extensible and added two implementations: 
    PionSingleServiceScheduler (a single service for multiple threads), and 
    PionOneToOneScheduler (a single service used for each thread).
    
    Greatly improved overall performance of PionScheduler; shutdown is also 
    now immediately instead of being delayed up to 1/4 second.  This makes 
    the unit tests run MANY times faster than before.
    
    Updated autogen.sh so that it looks for both glibtoolize and libtoolize 
    in both /usr/bin and /usr/local/bin.
    
    Updated README.osx to remove MacPorts documentation, since this is no 
    longer required (as of 10.5 Leopard), and since MacPorts seems to be 
    extremely buggy and unreliable.

--- autogen.sh
+++ autogen.sh
@@ -10,7 +10,18 @@
 aclocal -I common/build
 
 # Install libtool and related files
-libtoolize --force
+if test -f "/usr/local/bin/libtoolize"; then
+	/usr/local/bin/libtoolize --force
+elif test -f "/usr/local/bin/glibtoolize"; then
+	/usr/local/bin/glibtoolize --force
+elif test -f "/usr/bin/libtoolize"; then
+	/usr/bin/libtoolize --force
+elif test -f "/usr/bin/glibtoolize"; then
+	/usr/bin/glibtoolize --force
+else
+	echo "Error: could not find libtoolize"
+	exit
+fi
 
 # Generate configure script
 autoconf
