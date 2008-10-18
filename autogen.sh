4826f4d537449dadea31202a97adc09f942653c9
Michael Dickey <mike@atomiclabs.com>
2008-10-18 18:48:33 +0000

    http://trac.atomiclabs.com/ticket/347 pion-net updates for Tom's autoconf changes

--- autogen.sh
+++ autogen.sh
@@ -4,25 +4,8 @@
 #
 
 # DO NOT USE autoheader -> config.h.in file is NOT automanaged!!!
-#autoheader
-
-# create aclocal.m4
-aclocal -I common/build
-
-# Install libtool and related files
-if test -f "/usr/local/bin/libtoolize"; then
-	/usr/local/bin/libtoolize --force
-elif test -f "/usr/local/bin/glibtoolize"; then
-	/usr/local/bin/glibtoolize --force
-elif test -f "/usr/bin/libtoolize"; then
-	/usr/bin/libtoolize --force
-elif test -f "/usr/bin/glibtoolize"; then
-	/usr/bin/glibtoolize --force
-else
-	echo "Error: could not find libtoolize"
-	exit
-fi
+AUTOHEADER=`which true`
+export AUTOHEADER
 
 # Generate configure script
-autoconf
-automake -a
+autoreconf -ifs
