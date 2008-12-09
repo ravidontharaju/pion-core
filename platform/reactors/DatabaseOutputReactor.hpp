a945fdc57acc6164d44de72c65b0a5d2271fb6c7
Taneli Otala <taneli@atomiclabs.com>
2008-12-09 22:31:14 +0000

    DatabaseOutputReactor
    Reject field (column) names that don't conform to SQL92 character set
    by throwing an exception

--- platform/reactors/DatabaseOutputReactor.hpp
+++ platform/reactors/DatabaseOutputReactor.hpp
@@ -51,35 +51,35 @@ public:
 		EmptyDatabaseException(const std::string& reactor_id)
 			: PionException("DatabaseOutputReactor configuration is missing a required Database parameter: ", reactor_id) {}
 	};
-		
+
 	/// exception thrown if the DatabaseOutputReactor configuration does not define a Table
 	class EmptyTableException : public PionException {
 	public:
 		EmptyTableException(const std::string& reactor_id)
 			: PionException("DatabaseOutputReactor configuration is missing a required Table parameter: ", reactor_id) {}
 	};
-	
+
 	/// exception thrown if there are no database field mappings in the configuration
 	class NoFieldsException : public PionException {
 	public:
 		NoFieldsException(const std::string& reactor_id)
 			: PionException("DatabaseOutputReactor configuration must contain at least one field mapping: ", reactor_id) {}
 	};
-	
+
 	/// exception thrown if the DatabaseOutputReactor configuration includes an empty field name
 	class EmptyFieldException : public PionException {
 	public:
 		EmptyFieldException(const std::string& reactor_id)
 			: PionException("DatabaseOutputReactor configuration includes an empty field name: ", reactor_id) {}
 	};
-	
+
 	/// exception thrown if the DatabaseOutputReactor configuration does not define a term in a field mapping
 	class EmptyTermException : public PionException {
 	public:
 		EmptyTermException(const std::string& reactor_id)
 			: PionException("DatabaseOutputReactor configuration is missing a term identifier: ", reactor_id) {}
 	};
-	
+
 	/// exception thrown if the DatabaseOutputReactor configuration uses an unknown term in a field mapping
 	class UnknownTermException : public PionException {
 	public:
@@ -87,17 +87,22 @@ public:
 			: PionException("DatabaseOutputReactor configuration maps field to an unknown term: ", reactor_id) {}
 	};
 
-	
+	class IllegalCharactersException: public PionException {
+	public:
+		IllegalCharactersException(const std::string& reactor_id)
+			: PionException("DatabaseOutputReactor configuration has a field name with illegal characters: ", reactor_id) {}
+	};
+
 	/// constructs a new DatabaseOutputReactor object
 	DatabaseOutputReactor(void)
 		: pion::platform::Reactor(TYPE_STORAGE),
 		m_logger(PION_GET_LOGGER("pion.DatabaseOutputReactor")),
 		m_queue_max(DEFAULT_QUEUE_SIZE), m_queue_timeout(0), m_num_queued(0)
 	{}
-	
+
 	/// virtual destructor: this class is meant to be extended
 	virtual ~DatabaseOutputReactor() { stop(); }
-	
+
 	/**
 	 * sets configuration parameters for this Reactor
 	 *
@@ -106,7 +111,7 @@ public:
 	 *                   configuration parameters
 	 */
 	virtual void setConfig(const pion::platform::Vocabulary& v, const xmlNodePtr config_ptr);
-	
+
 	/**
 	 * this updates the Vocabulary information used by this Reactor; it should
 	 * be called whenever the global Vocabulary is updated
@@ -114,13 +119,13 @@ public:
 	 * @param v the Vocabulary that this Reactor will use to describe Terms
 	 */
 	virtual void updateVocabulary(const pion::platform::Vocabulary& v);
-	
+
 	/**
 	 * this updates the Databases that are used by this Reactor; it should
 	 * be called whenever any Database's configuration is updated
 	 */
 	virtual void updateDatabases(void);
-	
+
 	/**
 	 * processes an Event by comparing its data to the configured RuleChain.
 	 * Only Events which pass all Comparisons in the RuleChain will be
@@ -129,7 +134,7 @@ public:
 	 * @param e pointer to the Event to process
 	 */
 	virtual void operator()(const pion::platform::EventPtr& e);
-		
+
 	/**
 	 * handle an HTTP query (from QueryService)
 	 *
@@ -144,33 +149,33 @@ public:
 
 	/// called by the ReactorEngine to start Event processing
 	virtual void start(void);
-	
+
 	/// called by the ReactorEngine to stop Event processing
 	virtual void stop(void);
-	
+
 	/// sets the logger to be used
 	inline void setLogger(PionLogger log_ptr) { m_logger = log_ptr; }
-	
+
 	/// returns the logger currently in use
 	inline PionLogger getLogger(void) { return m_logger; }
 
-	
+
 private:
-	
+
 	/// function used by the writer thread to store events to the database
 	void insertEvents(void);
-	
-	
+
+
 	/// data type for a collection of queued Events
 	typedef std::vector<pion::platform::EventPtr>	EventQueue;
 
-	
+
 	/// default maximum number of events that may be queued for insertion
 	static const boost::uint32_t			DEFAULT_QUEUE_SIZE;
-	
+
 	/// default number of seconds before the queue is automatically flushed due to timeout
 	static const boost::uint32_t			DEFAULT_QUEUE_TIMEOUT;
-	
+
 	/// name of the database element for Pion XML config files
 	static const std::string				DATABASE_ELEMENT_NAME;
 
@@ -179,20 +184,22 @@ private:
 
 	/// name of the field element for Pion XML config files
 	static const std::string				FIELD_ELEMENT_NAME;
-	
+
 	/// name of the queue size element for Pion XML config files
 	static const std::string				QUEUE_SIZE_ELEMENT_NAME;
-	
+
 	/// name of the queue timeout element for Pion XML config files
 	static const std::string				QUEUE_TIMEOUT_ELEMENT_NAME;
-	
+
 	/// name of the events queued element for Pion XML config files
 	static const std::string				EVENTS_QUEUED_ELEMENT_NAME;
-	
-	/// name of the Term ID attribute for Pion XML config files
-	static const std::string				TERM_ATTRIBUTE_NAME;	
 
-	
+	/// name of the Term ID attribute for Pion XML config files
+	static const std::string				TERM_ATTRIBUTE_NAME;
+
+	/// legal character set for SQL92 column/table names
+	static const char *						CHARSET_FOR_TABLES;
+
 	/// primary logging interface used by this class
 	PionLogger								m_logger;
 
@@ -201,28 +208,28 @@ private:
 
 	/// name of the table into which events will be stored
 	std::string								m_table_name;
-	
+
 	/// maps Term references to database field names
 	pion::platform::Query::FieldMap			m_field_map;
 
 	/// pointer to the database that is used to store events
 	pion::platform::DatabasePtr				m_database_ptr;
-	
+
 	/// collection of events queued for storage to the database
 	EventQueue								m_event_queue;
-	
+
 	/// maximum number of events that may be queued for insertion
 	boost::uint32_t							m_queue_max;
-	
+
 	/// number of seconds before the queue is automatically flushed due to timeout
 	boost::uint32_t							m_queue_timeout;
 
 	/// number of events that are currently queued for storage to the database
 	boost::uint32_t							m_num_queued;
-	
+
 	/// condition triggered to notify the writer thread to save events to the database
 	boost::condition						m_wakeup_writer;
-	
+
 	/// condition triggered to notify all threads that the the queue was flushed
 	boost::condition						m_flushed_queue;
 
