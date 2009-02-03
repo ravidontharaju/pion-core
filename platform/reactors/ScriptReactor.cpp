7a62c6739e1c866e496f3ab7293ef01240bff34a
Michael Dickey <mike@atomiclabs.com>
2009-02-03 00:44:14 +0000

    Checking-in initial implementation of ScriptReactor (probably Unix-only right now)

---
+++ platform/reactors/ScriptReactor.cpp
@@ -0,0 +1,252 @@
+// ------------------------------------------------------------------------
+// Pion is a development platform for building Reactors that process Events
+// ------------------------------------------------------------------------
+// Copyright (C) 2007-2009 Atomic Labs, Inc.  (http://www.atomiclabs.com)
+//
+// Pion is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Pion is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Pion.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <iostream>
+#include <sys/select.h>
+#include <pion/platform/CodecFactory.hpp>
+#include "ScriptReactor.hpp"
+
+using namespace pion::platform;
+
+
+namespace pion {		// begin namespace pion
+namespace plugins {		// begin namespace plugins
+
+
+// static members of ScriptReactor
+
+const std::string		ScriptReactor::INPUT_CODEC_ELEMENT_NAME = "InputCodec";
+const std::string		ScriptReactor::OUTPUT_CODEC_ELEMENT_NAME = "OutputCodec";
+const std::string		ScriptReactor::COMMAND_ELEMENT_NAME = "Command";
+
+
+// ScriptReactor member functions
+
+void ScriptReactor::start(void)
+{
+	ConfigWriteLock cfg_lock(*this);
+	if (! m_is_running) {
+		// open pipe to script
+		openPipe();
+		
+		m_is_running = true;
+
+		// spawn a new thread that will be used to read events from the script
+		PION_LOG_DEBUG(m_logger, "Starting reader thread: " << getId());
+		m_thread_ptr.reset(new boost::thread(boost::bind(&ScriptReactor::readEvents, this)));
+	}
+}
+	
+void ScriptReactor::stop(void)
+{
+	ConfigWriteLock cfg_lock(*this);
+	if (m_is_running) {
+		closePipe();
+		m_is_running = false;
+		m_thread_ptr->join();
+	}
+}
+
+void ScriptReactor::setConfig(const Vocabulary& v, const xmlNodePtr config_ptr)
+{
+	// first set config options for the Reactor base class
+	ConfigWriteLock cfg_lock(*this);
+	Reactor::setConfig(v, config_ptr);
+	
+	// get the Input Codec that the Reactor should use
+	if (! ConfigManager::getConfigOption(INPUT_CODEC_ELEMENT_NAME, m_input_codec_id, config_ptr))
+		throw EmptyInputCodecException(getId());
+	m_input_codec_ptr = getCodecFactory().getCodec(m_input_codec_id);
+	PION_ASSERT(m_input_codec_ptr);
+
+	// get the Output Codec that the Reactor should use
+	if (! ConfigManager::getConfigOption(OUTPUT_CODEC_ELEMENT_NAME, m_output_codec_id, config_ptr))
+		throw EmptyOutputCodecException(getId());
+	m_output_codec_ptr = getCodecFactory().getCodec(m_output_codec_id);
+	PION_ASSERT(m_output_codec_ptr);
+	
+	// get the script or program command to execute
+	if (! ConfigManager::getConfigOption(COMMAND_ELEMENT_NAME, m_command, config_ptr))
+		throw EmptyCommandException(getId());
+}
+	
+void ScriptReactor::updateVocabulary(const Vocabulary& v)
+{
+	// first update anything in the Reactor base class that might be needed
+	ConfigWriteLock cfg_lock(*this);
+	Reactor::updateVocabulary(v);
+
+	if (m_input_codec_ptr)
+		m_input_codec_ptr->updateVocabulary(v);
+
+	if (m_output_codec_ptr)
+		m_output_codec_ptr->updateVocabulary(v);
+}
+	
+void ScriptReactor::updateCodecs(void)
+{
+	// check if a codec was deleted (if so, stop now!)
+	if (! getCodecFactory().hasPlugin(m_input_codec_id)
+		|| ! getCodecFactory().hasPlugin(m_output_codec_id))
+	{
+		stop();
+	} else {
+		// update the codec pointer
+		ConfigWriteLock cfg_lock(*this);
+		m_input_codec_ptr = getCodecFactory().getCodec(m_input_codec_id);
+		m_output_codec_ptr = getCodecFactory().getCodec(m_output_codec_id);
+	}
+}
+	
+void ScriptReactor::process(const EventPtr& e)
+{
+	PION_ASSERT(m_input_stream_ptr);
+	PION_ASSERT(m_input_stream_ptr->is_open());
+	PION_ASSERT(m_input_codec_ptr);
+
+	// lock mutex to ensure that only one Event may be written at a time
+	boost::mutex::scoped_lock write_lock(m_write_mutex);
+	
+	// write the Event to the pipe
+	m_input_codec_ptr->write(*m_input_stream_ptr, *e);
+	if (! *m_input_stream_ptr)
+		throw WriteToPipeException(getId());
+		
+	// unlock mutex after writing to pipe
+	write_lock.unlock();
+
+	// note: delivery to other reactors is handled by readEvents()
+}
+
+void ScriptReactor::readEvents(void)
+{
+	PION_LOG_DEBUG(m_logger, "Script reader thread is running: " << getId());
+
+	try {
+	
+		const Event::EventType event_type(m_output_codec_ptr->getEventType());
+		EventFactory event_factory;
+		EventPtr event_ptr;
+		bool event_read;
+
+		const int pipe_fd = ::fileno(m_pipe);
+		struct timeval timeout;
+		fd_set read_fds;
+		
+		while ( isRunning() ) {
+		
+			PION_ASSERT(m_output_stream_ptr);
+			PION_ASSERT(m_output_stream_ptr->is_open());
+			PION_ASSERT(m_output_code_ptr);
+
+			// these need to be reset before each select()
+			FD_ZERO(&read_fds);
+			FD_SET(pipe_fd, &read_fds);
+			timeout.tv_sec = 0;
+			timeout.tv_usec = 100000;	// microseconds (1/10 second)
+
+			// wait for data to be available
+			if ( ::select(pipe_fd+1, &read_fds, NULL, NULL, &timeout) == 1 ) {
+				if ( ! isRunning() )	// re-check after sleep
+					break;
+				ConfigReadLock cfg_lock(*this);
+				if ( ! isRunning() )	// re-check after locking
+					break;
+
+				// get a new event from the EventFactory
+				event_factory.create(event_ptr, event_type);
+				
+				// read an event using the output codec
+				event_read = m_output_codec_ptr->read(*m_output_stream_ptr, *event_ptr);
+	
+				if (event_read) {
+	
+					// deliver the Event to other Reactors
+					deliverEvent(event_ptr);
+	
+				} else {
+				
+					// check for read error
+					if ( isRunning() )
+						throw ReadFromPipeException(getId());
+				}
+			}
+		}
+
+	} catch (std::exception& e) {
+		PION_LOG_FATAL(m_logger, e.what());
+		ConfigWriteLock cfg_lock(*this);
+		if ( isRunning() ) {
+			closePipe();
+			m_is_running = false;
+		}
+	}
+
+	PION_LOG_DEBUG(m_logger, "Script reader thread is exiting: " << getId());
+}	
+	
+void ScriptReactor::openPipe(void)
+{
+	// close first if already open
+	closePipe();
+	
+	PION_LOG_DEBUG(m_logger, "Opening pipe to command: " << m_command);
+
+	// open pipe to script
+	m_pipe = ::popen(m_command.c_str(), "r+");
+	if (m_pipe == NULL)
+		throw OpenPipeException(m_command);
+		
+	// disable buffering for the pipe
+	::setvbuf(m_pipe, NULL, _IONBF, BUFSIZ);
+	
+	// prepare c++ streams to use pipe for reading and writing events
+	// (yes, it's necessary to use separate ones for input and output..)
+	m_input_streambuf_ptr.reset(new StreamBuffer( ::fileno(m_pipe) ));
+	m_input_stream_ptr.reset(new std::ostream(m_input_streambuf_ptr.get()));
+	m_output_streambuf_ptr.reset(new StreamBuffer( ::fileno(m_pipe) ));
+	m_output_stream_ptr.reset(new std::istream(m_output_streambuf_ptr.get()));
+}
+
+void ScriptReactor::closePipe(void)
+{
+	if (m_pipe) {
+		PION_LOG_DEBUG(m_logger, "Closing pipe to command: " << m_command);
+		::pclose(m_pipe);
+		m_input_stream_ptr.reset();
+		m_input_streambuf_ptr.reset();
+		m_output_stream_ptr.reset();
+		m_output_streambuf_ptr.reset();
+		m_pipe = NULL;
+	}
+}
+
+}	// end namespace plugins
+}	// end namespace pion
+
+
+/// creates new ScriptReactor objects
+extern "C" PION_PLUGIN_API pion::platform::Reactor *pion_create_ScriptReactor(void) {
+	return new pion::plugins::ScriptReactor();
+}
+
+/// destroys ScriptReactor objects
+extern "C" PION_PLUGIN_API void pion_destroy_ScriptReactor(pion::plugins::ScriptReactor *reactor_ptr) {
+	delete reactor_ptr;
+}
---
+++ platform/reactors/ScriptReactor.hpp
@@ -0,0 +1,213 @@
+// ------------------------------------------------------------------------
+// Pion is a development platform for building Reactors that process Events
+// ------------------------------------------------------------------------
+// Copyright (C) 2007-2009 Atomic Labs, Inc.  (http://www.atomiclabs.com)
+//
+// Pion is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Pion is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Pion.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __PION_SCRIPTREACTOR_HEADER__
+#define __PION_SCRIPTREACTOR_HEADER__
+
+#include <cstdio>
+#include <iosfwd>
+#include <boost/scoped_ptr.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <pion/PionConfig.hpp>
+#include <pion/PionLogger.hpp>
+#include <pion/PionException.hpp>
+#include <pion/platform/Codec.hpp>
+#include <pion/platform/Reactor.hpp>
+
+
+namespace pion {		// begin namespace pion
+namespace plugins {		// begin namespace plugins
+
+
+///
+/// ScriptReactor: interfaces with an external shell script or program
+///
+class ScriptReactor :
+	public pion::platform::Reactor
+{
+public:	
+	
+	/// exception thrown if the Reactor configuration does not define an Input Codec
+	class EmptyInputCodecException : public PionException {
+	public:
+		EmptyInputCodecException(const std::string& reactor_id)
+			: PionException("ScriptReactor configuration is missing a required InputCodec parameter: ", reactor_id) {}
+	};
+	
+	/// exception thrown if the Reactor configuration does not define an Output Codec
+	class EmptyOutputCodecException : public PionException {
+	public:
+		EmptyOutputCodecException(const std::string& reactor_id)
+			: PionException("ScriptReactor configuration is missing a required OutputCodec parameter: ", reactor_id) {}
+	};
+	
+	/// exception thrown if the Reactor configuration does not define a Command
+	class EmptyCommandException : public PionException {
+	public:
+		EmptyCommandException(const std::string& reactor_id)
+			: PionException("ScriptReactor configuration is missing a required Command parameter: ", reactor_id) {}
+	};
+
+	/// exception thrown if opening a pipe to the shell command fails
+	class OpenPipeException : public PionException {
+	public:
+		OpenPipeException(const std::string& command)
+			: PionException("ScriptReactor failed to open pipe: ", command) {}
+	};
+
+	/// exception thrown if the Reactor has trouble reading events from the pipe
+	class ReadFromPipeException : public PionException {
+	public:
+		ReadFromPipeException(const std::string& reactor_id)
+			: PionException("ScriptReactor failed reading event from pipe: ", reactor_id) {}
+	};
+
+	/// exception thrown if the Reactor has trouble writing events to the pipe
+	class WriteToPipeException : public PionException {
+	public:
+		WriteToPipeException(const std::string& reactor_id)
+			: PionException("ScriptReactor failed writing an event to pipe: ", reactor_id) {}
+	};
+
+
+	/// constructs a new ScriptReactor object
+	ScriptReactor(void)
+		: Reactor(TYPE_PROCESSING),
+		m_logger(PION_GET_LOGGER("pion.ScriptReactor")), m_pipe(NULL)
+	{}
+	
+	/// virtual destructor: this class is meant to be extended
+	virtual ~ScriptReactor() { stop(); }
+	
+	/// called by the ReactorEngine to start Event processing
+	virtual void start(void);
+	
+	/// called by the ReactorEngine to stop Event processing
+	virtual void stop(void);
+	
+	/**
+	 * sets configuration parameters for this Reactor
+	 *
+	 * @param v the Vocabulary that this Reactor will use to describe Terms
+	 * @param config_ptr pointer to a list of XML nodes containing Reactor
+	 *                   configuration parameters
+	 */
+	virtual void setConfig(const pion::platform::Vocabulary& v, const xmlNodePtr config_ptr);
+	
+	/**
+	 * this updates the Vocabulary information used by this Reactor; it should
+	 * be called whenever the global Vocabulary is updated
+	 *
+	 * @param v the Vocabulary that this Reactor will use to describe Terms
+	 */
+	virtual void updateVocabulary(const pion::platform::Vocabulary& v);
+	
+	/**
+	 * this updates the Codecs that are used by this Reactor; it should
+	 * be called whenever any Codec's configuration is updated
+	 */
+	virtual void updateCodecs(void);
+	
+	/**
+	 * processes an Event by delivering it to the shell script or program
+	 *
+	 * @param e pointer to the Event to process
+	 */
+	virtual void process(const pion::platform::EventPtr& e);
+	
+	/// sets the logger to be used
+	inline void setLogger(PionLogger log_ptr) { m_logger = log_ptr; }
+	
+	/// returns the logger currently in use
+	inline PionLogger getLogger(void) { return m_logger; }
+	
+	
+private:
+
+	/// thread function to read events from the script command
+	void readEvents(void);
+	
+	/// opens a pipe to the script command
+	void openPipe(void);
+
+	/// closes the pipe to the script command	
+	void closePipe(void);
+	
+	
+	/// name of the InputCodec element for Pion XML config files
+	static const std::string			INPUT_CODEC_ELEMENT_NAME;
+	
+	/// name of the OutputCodec element for Pion XML config files
+	static const std::string			OUTPUT_CODEC_ELEMENT_NAME;
+	
+	/// name of the Command element for Pion XML config files
+	static const std::string			COMMAND_ELEMENT_NAME;
+	
+
+	/// data type for an iostreams streambuf that uses a C-style file descriptor
+	typedef boost::iostreams::stream_buffer<boost::iostreams::file_descriptor>	StreamBuffer;
+	
+
+	/// primary logging interface used by this class
+	PionLogger							m_logger;
+	
+	/// pointer to the Codec that is used for writing Events to the script
+	pion::platform::CodecPtr			m_input_codec_ptr;
+	
+	/// pointer to the Codec that is used for reading Events from the script
+	pion::platform::CodecPtr			m_output_codec_ptr;
+	
+	/// unique identifier of the Codec that is used for writing Events
+	std::string							m_input_codec_id;
+	
+	/// unique identifier of the Codec that is used for reading Events
+	std::string							m_output_codec_id;
+	
+	/// name of the shell script or program to execute
+	std::string							m_command;
+	
+	/// pipe to the shell script or program
+	FILE *								m_pipe;
+	
+	/// pointer to a C++ stream buffer used to write Events to the pipe
+	boost::scoped_ptr<StreamBuffer>		m_input_streambuf_ptr;
+	
+	/// pointer to a C++ stream buffer used to read Events from the pipe
+	boost::scoped_ptr<StreamBuffer>		m_output_streambuf_ptr;
+	
+	/// pointer to a C++ iostream used to write Events to the pipe
+	boost::scoped_ptr<std::ostream>		m_input_stream_ptr;
+	
+	/// pointer to a C++ iostream used to read Events from the pipe
+	boost::scoped_ptr<std::istream>		m_output_stream_ptr;
+	
+	/// thread used to read events generated by the script
+	boost::scoped_ptr<boost::thread>	m_thread_ptr;
+
+	/// used to ensure only event may be written at a time
+	boost::mutex						m_write_mutex;
+};
+
+
+}	// end namespace plugins
+}	// end namespace pion
+
+#endif
