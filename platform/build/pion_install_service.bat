f1c3398f7d5436249c3144bc565aac0a4056203e
Vladimir Shcherbakov <vladimir@atomiclabs.com>
2009-06-23 05:39:02 +0000

    http://trac.atomiclabs.com/ticket/276 Windows installer: added Windows service support

---
+++ platform/build/pion_install_service.bat
@@ -0,0 +1,1 @@
+nssm install2cons "pion-community" "Atomic Labs Pion (Community Edition)" "Pion is your friendly neighborhood complex event processing solution." "{APPDIR}pion.exe" "-c config\platform.xml"
---
+++ platform/build/pion_uninstall_service.bat
@@ -0,0 +1,1 @@
+nssm remove "pion-community" confirm
