46c670fc2e38731c15a520ae6a17c21548f3f52d
Michael Dickey <mike@atomiclabs.com>
2009-09-14 02:40:10 +0000

    http://trac.atomiclabs.com/ticket/651
    Added support for email and restart parameters

--- platform/build/pmon.py
+++ platform/build/pmon.py
@@ -3,7 +3,15 @@
 # pion server monitor script
 # --------------------------
 
-import pget, sys, time, commands
+import pget, sys, time, commands, smtplib
+
+
+# these parameters control how email alert messages are sent
+SMTP_SENDER = 'pion@localhost'
+SMTP_SERVER = 'localhost'
+SMTP_PORT = 25
+SMTP_USER = ''
+SMTP_PASSWORD = ''
 
 
 # defines a mapping of reactor statistics to maximum threshold values
@@ -62,11 +70,26 @@ def get_metrics(con, options, reactors):
 	return metrics
 
 
+def send_email(recipients, last_metrics, msg):
+	s = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+	if (SMTP_USER != ''):
+		s.login(SMTP_USER, SMTP_PASSWORD)
+	contents = 'From: ' + SMTP_SENDER + '\nTo: ' + recipients + '\nSubject: pmon alert: ' + msg + '\n\n' + last_metrics
+	result = s.sendmail(SMTP_SENDER, recipients, contents)
+	if (result):
+		errmsg = "Unable to send email to %s" % recipients
+		raise smtplib.SMTPException, errmsg
+
+
 def main():
 	# parse command-line options
 	parser = pget.get_arg_parser()
 	parser.add_option("-c", "--command", action="store",
 		help="command to execute at the end of each interval")
+	parser.add_option("-e", "--email", action="store",
+		help="email address(es) to which alerts will be sent")
+	parser.add_option("", "--restart", action="store",
+		help="command executed to restart Pion if connection is lost")
 	options, arguments = parser.parse_args()		
 	# argument sanity check
 	options.interval = None
@@ -78,28 +101,84 @@ def main():
 		except:
 			print 'error: bad interval argument'
 			sys.exit(1)
-	# establish connection to Pion server
-	con = pget.get_con(options)
-	# retrieve a collection of reactor objects from pion config
-	reactors = pget.get_reactors(con, options)
 	# main loop
+	initialized = False
+	restarting = False
 	while (True):
-		print "------------------------------------------"
-		print '|', time.strftime("PION STATISTICS AT %Y-%m-%d %H:%M:%S"), '|'
-		print "------------------------------------------"
+		if (not initialized):
+			last_metrics = ""
+			last_metrics_error = False
+			try:
+				# establish connection to Pion server
+				con = pget.get_con(options)
+				# retrieve a collection of reactor objects from pion config
+				reactors = pget.get_reactors(con, options)
+			except KeyboardInterrupt:
+				raise
+			except:
+				if (restarting):
+					print 'FATAL: unable to restart Pion server!'
+					if (options.email):
+						send_email(options.email, last_metrics, 'unable to restart Pion server!')
+				raise
+			initialized = True
+			restarting = False
 		# retrieve stats for all reactors we want to monitor
-		metrics = get_metrics(con, options, reactors)
+		try:
+			metrics = get_metrics(con, options, reactors)
+		except KeyboardInterrupt:
+			raise
+		except:
+			if (last_metrics == ""):
+				raise
+			print 'error: lost connection to Pion server!'
+			if (options.restart):
+				result = commands.getoutput(options.restart)
+				if (options.email):
+					try:
+						send_email(options.email, last_metrics, 'lost connection, restarting Pion!')
+					except KeyboardInterrupt:
+						raise
+					except:
+						# don't shutdown just because email fails
+						print 'error: unable to send alert email'
+				# wait for pion to finish starting up
+				time.sleep(options.interval)
+				# force re-initialization
+				initialized = False
+				restarting = True
+				continue
+			elif (options.email):
+				send_email(options.email, last_metrics, 'lost connection to Pion server!')
+			sys.exit(1)
 		metrics.sort()
+		# generate summary of metrics collected
+		last_metrics = "------------------------------------------\n"
+		last_metrics += '| ' + time.strftime("PION STATISTICS AT %Y-%m-%d %H:%M:%S") + ' |\n'
+		last_metrics += "------------------------------------------\n"
+		metrics_error = False
 		for m in metrics:
-			print m
+			if (not metrics_error and m.value > m.max):
+				metrics_error = True
+				last_metrics += '*'
+			last_metrics += str(m) + '\n'
 		if (options.command):
-			print commands.getoutput(options.command)
+			last_metrics += '\n' + commands.getoutput(options.command)
+		# display summary
+		print last_metrics
+		# handle metrics out of range
+		if (options.email):
+			if (metrics_error):
+				if (not last_metrics_error):
+					send_email(options.email, last_metrics, 'metrics range exceeded!')
+			elif (last_metrics_error):
+				send_email(options.email, last_metrics, 'metrics returned to normal')
+			last_metrics_error = metrics_error
+		# all done if no interval defined
 		if (not options.interval):
 			break
 		# sleep a bit
 		time.sleep(options.interval)
-	# close the HTTP connection
-	con.close()
 
 
 # call main() if script is being executed	
