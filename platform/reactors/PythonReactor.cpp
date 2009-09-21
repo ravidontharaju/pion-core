58b52ed97dce28025d955c7d382db5efdcf8a9d2
Michael Dickey <mike@atomiclabs.com>
2009-09-21 23:14:42 +0000

    PythonReactor - adding patch for Python < 2.5 (now builds on RHEL5)

--- platform/reactors/PythonReactor.cpp
+++ platform/reactors/PythonReactor.cpp
@@ -27,6 +27,12 @@
 #include <pion/platform/ConfigManager.hpp>
 #include "PythonReactor.hpp"
 
+// for compatibility with Python < 2.5
+#if PY_VERSION_HEX < 0x02050000
+	typedef int Py_ssize_t;
+	#define PY_SSIZE_T_MAX INT_MAX
+	#define PY_SSIZE_T_MIN INT_MIN
+#endif
 
 using namespace std;
 using namespace pion::platform;
