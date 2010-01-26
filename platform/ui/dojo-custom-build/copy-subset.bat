c6ec94a8c486d721c334a5378f3f6120b739a9ba
Mignon Belongie <mignon@atomiclabs.com>
2010-01-26 17:40:58 +0000

    Added some files needed for UI unit tests.

--- platform/ui/dojo-custom-build/copy-subset.bat
+++ platform/ui/dojo-custom-build/copy-subset.bat
@@ -2,26 +2,26 @@ rem Copy a subset of dojo-release (containing all needed files) onto the local d
 
 set releaseDir=C:\dojo-release-1.3.2-src\release\dojo-release
 
-xcopy /y /s %releaseDir%\dojo\resources          ..\dojo-release\dojo\resources
-copy /y %releaseDir%\dojo\nls\pion-dojo_*.js     ..\dojo-release\dojo\nls
-copy /y %releaseDir%\dojo\dojo.js                ..\dojo-release\dojo\dojo.js
-copy /y %releaseDir%\dojo\pion-dojo.js           ..\dojo-release\dojo\pion-dojo.js
-copy /y %releaseDir%\dojo\build.txt              ..\dojo-release\dojo\build.txt
-copy /y %releaseDir%\dojo\LICENSE                ..\dojo-release\dojo\LICENSE
-copy /y %releaseDir%\dojo\_firebug               ..\dojo-release\dojo\_firebug
+xcopy /y /s %releaseDir%\dojo\resources            ..\dojo-release\dojo\resources
+copy /y %releaseDir%\dojo\nls\pion-dojo_*.js       ..\dojo-release\dojo\nls
+copy /y %releaseDir%\dojo\dojo.js                  ..\dojo-release\dojo\dojo.js
+copy /y %releaseDir%\dojo\pion-dojo.js             ..\dojo-release\dojo\pion-dojo.js
+copy /y %releaseDir%\dojo\build.txt                ..\dojo-release\dojo\build.txt
+copy /y %releaseDir%\dojo\LICENSE                  ..\dojo-release\dojo\LICENSE
+copy /y %releaseDir%\dojo\_firebug                 ..\dojo-release\dojo\_firebug
 
-copy /y %releaseDir%\dijit\templates             ..\dojo-release\dijit\templates
-copy /y %releaseDir%\dijit\themes\*.css          ..\dojo-release\dijit\themes
-xcopy /y /s %releaseDir%\dijit\themes\tundra     ..\dojo-release\dijit\themes\tundra
-copy /y %releaseDir%\dijit\themes\a11y           ..\dojo-release\dijit\themes\a11y
+copy /y %releaseDir%\dijit\templates               ..\dojo-release\dijit\templates
+copy /y %releaseDir%\dijit\themes\*.css            ..\dojo-release\dijit\themes
+xcopy /y /s %releaseDir%\dijit\themes\tundra       ..\dojo-release\dijit\themes\tundra
+copy /y %releaseDir%\dijit\themes\a11y             ..\dojo-release\dijit\themes\a11y
 
-copy /y %releaseDir%\dojox\gfx                              ..\dojo-release\dojox\gfx
-copy /y %releaseDir%\dojox\grid\compat\_grid\tundraGrid.css ..\dojo-release\dojox\grid\compat\_grid\tundraGrid.css
-copy /y %releaseDir%\dojox\grid\compat\_grid\images         ..\dojo-release\dojox\grid\compat\_grid\images
-xcopy /y /s %releaseDir%\dojox\grid\resources               ..\dojo-release\dojox\grid\resources
-copy /y %releaseDir%\dojox\data\QueryReadStore.js           ..\dojo-release\dojox\data\QueryReadStore.js
+copy /y %releaseDir%\dojox\gfx                     ..\dojo-release\dojox\gfx
+xcopy /y /s %releaseDir%\dojox\grid\resources      ..\dojo-release\dojox\grid\resources
+copy /y %releaseDir%\dojox\data\QueryReadStore.js  ..\dojo-release\dojox\data\QueryReadStore.js
 
-rem The remaining files are currently only needed for AggregateReactor.
+xcopy /y /s %releaseDir%\util\doh                  ..\dojo-release\util\doh
+
+rem The remaining files are currently only needed for ReplayService and AggregateReactor.
 copy /y %releaseDir%\dojox\html\_base.js                      ..\dojo-release\dojox\html\_base.js
 copy /y %releaseDir%\dojox\layout\ContentPane.js              ..\dojo-release\dojox\layout\ContentPane.js
 copy /y %releaseDir%\dojox\layout\FloatingPane.js             ..\dojo-release\dojox\layout\FloatingPane.js
