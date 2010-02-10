afeebd4d9a32a2fb50df32430d379333943d7965
Mignon Belongie <mignon@atomiclabs.com>
2010-02-10 20:35:35 +0000

    http://trac.atomiclabs.com/ticket/842 Added check boxes for RawRequestHeaders and RawResponseHeaders in HTTPProtocol UI.

--- platform/ui/plugins/protocols/HTTPProtocol.js
+++ platform/ui/plugins/protocols/HTTPProtocol.js
@@ -22,6 +22,30 @@ dojo.declare("plugins.protocols.HTTPProtocolPane",
 		getHeight: function() {
 			// TODO: replace 525 with some computed value
 			return 525;
+		},
+		_addCustomConfigValues: function(config, item) {
+			var store = pion.codecs.config_store;
+			config.options = []; // By default, RawRequestHeaders and RawResponseHeaders are both false.
+
+			// Override defaults if options present in the configuration.
+			if (store.hasAttribute(item, 'RawRequestHeaders')) {
+				if (store.getValue(item, 'RawRequestHeaders').toString() == 'true') {
+					config.options.push('RawRequestHeaders');
+				}
+			}
+			if (store.hasAttribute(item, 'RawResponseHeaders')) {
+				if (store.getValue(item, 'RawResponseHeaders').toString() == 'true') {
+					config.options.push('RawResponseHeaders');
+				}
+			}
+		},
+		_makeCustomElements: function(config) {
+			var put_data = '<RawRequestHeaders>';
+			put_data += (dojo.indexOf(config.options, 'RawRequestHeaders') != -1); // 'true' iff corresponding checkbox was checked
+			put_data += '</RawRequestHeaders><RawResponseHeaders>';
+			put_data += (dojo.indexOf(config.options, 'RawResponseHeaders') != -1); // 'true' iff corresponding checkbox was checked
+			put_data += '</RawResponseHeaders>';
+			return put_data;
 		}
 	}
 );
