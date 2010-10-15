a443a41276ca3a649af231cbd8e58fdb4a442a33
Taneli Otala <taneli@atomiclabs.com>
2010-10-15 01:17:28 +0000

    XMLLogService -- WIP
    Functional version of XMLLogService

--- platform/services/XMLLogService.hpp
+++ platform/services/XMLLogService.hpp
@@ -17,29 +17,15 @@
 #include <pion/net/WebService.hpp>
 #include <pion/net/HTTPResponseWriter.hpp>
 #include "PlatformConfig.hpp"
+#include "PlatformService.hpp"
 #include <string>
 #include <list>
 #include <deque>
 #include <queue>
 
-#if defined(PION_USE_LOG4CXX)
-	#include <log4cxx/appenderskeleton.h>
-	// version 0.10.x introduces a new data type that is declared in a
-	// pool.h header file.  If we're using 0.9.x, just declare the type
-	// as an int since it is not being used at all
-	#ifndef _LOG4CXX_HELPERS_POOL_H
-		namespace log4cxx {
-			namespace helpers {
-				typedef int Pool;
-			}
-		}
-	#endif
-#elif defined(PION_USE_LOG4CPLUS)
-	#include <log4cplus/appender.h>
-	#include <log4cplus/loglevel.h>
-#elif defined(PION_USE_LOG4CPP)
-	#include <log4cpp/AppenderSkeleton.hh>
-#endif
+// This only works with LOG4CPLUS
+#include <log4cplus/appender.h>
+#include <log4cplus/loglevel.h>
 
 
 namespace pion {		// begin namespace pion
@@ -50,13 +36,7 @@ namespace plugins {		// begin namespace plugins
 /// XMLLogServiceAppender: caches log events in memory for use by XMLLogService
 /// 
 class XMLLogServiceAppender
-	#if defined(PION_USE_LOG4CXX)
-		: public log4cxx::AppenderSkeleton
-	#elif defined(PION_USE_LOG4CPLUS)
 		: public log4cplus::Appender
-	#elif defined(PION_USE_LOG4CPP)
-		: public log4cpp::AppenderSkeleton
-	#endif
 {
 public:
 	// default constructor and destructor
@@ -66,9 +46,6 @@ public:
 	/// sets the maximum number of log events cached in memory
 	inline void setMaxEvents(unsigned int n) { m_max_events = n; }
 	
-	/// adds a formatted log message to the memory cache
-	void addLogString(const std::string& log_string);
-
 	/// writes the events cached in memory to a response stream
 	void writeLogEvents(pion::net::HTTPResponseWriterPtr& writer);
 
@@ -92,21 +69,6 @@ private:
 	/// mutex to make class thread-safe
 	boost::mutex							m_log_mutex;
 
-#if defined(PION_USE_LOG4CXX)
-	public:
-		// member functions inherited from the Appender interface class
-		virtual void close() {}
-		virtual bool requiresLayout() const { return false; }
-	protected:
-		/// adds log event to the memory cache
-		virtual void append(const log4cxx::spi::LoggingEventPtr& event);
-		// version 0.10.x adds a second "pool" argument -> just ignore it
-		virtual void append(const log4cxx::spi::LoggingEventPtr& event,
-							log4cxx::helpers::Pool& pool)
-		{
-			append(event);
-		}
-#elif defined(PION_USE_LOG4CPLUS)
 	public:
 		// member functions inherited from the Appender interface class
 		virtual void close() {}
@@ -115,20 +77,6 @@ private:
 	private:
 		/// this is used to convert numeric log levels into strings
 		log4cplus::LogLevelManager		m_log_level_manager;
-#elif defined(PION_USE_LOG4CPP)
-	public:
-		// member functions inherited from the AppenderSkeleton class
-		virtual void close() {}
-		virtual bool requiresLayout() const { return true; }
-		virtual void setLayout(log4cpp::Layout* layout) { m_layout_ptr.reset(layout); }
-	protected:
-		/// adds log event to the memory cache
-		virtual void _append(const log4cpp::LoggingEvent& event);
-	private:
-		/// the logging layout used to format events
-		boost::scoped_ptr<log4cpp::Layout>		m_layout_ptr;
-#endif
-
 };
 
 
@@ -136,7 +84,7 @@ private:
 /// XMLLogService: web service that displays log messages
 /// 
 class XMLLogService :
-	public pion::net::WebService
+	public pion::server::PlatformService
 {
 public:
 	// default constructor and destructor
