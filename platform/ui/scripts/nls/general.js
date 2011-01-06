25b3ce242e822a71161a95aacdc51384342aa260
Mignon Belongie <mignon@atomiclabs.com>
2011-01-06 17:50:27 +0000

    http://trac.atomiclabs.com/ticket/1010 Merged r2193 from 3.1.x/platform. (Added Keith's help labels and icons.)

--- platform/ui/scripts/nls/general.js
+++ platform/ui/scripts/nls/general.js
@@ -1,8 +1,8 @@
 ({
-	"default_header_help_label": "Help 1",
-	"default_nested_header_help_label": "<img src='../images/key.png' />",
-	"default_small_help_label": "Help 3",
-	"default_reactor_help_label": "Help 4",
-	"default_grid_header_help_label": "Help 5",
+	"default_header_help_label": "<span style='text-decoration: underline;'>Help</span> <img src='../images/icon-help-16-16.png' />",
+	"default_nested_header_help_label": "<img src='../images/icon-help-16-16.png' />",
+	"default_small_help_label": "<img src='../images/icon-help-12-12.png' style='margin-left: -10px;' />",
+	"default_reactor_help_label": "<img src='../images/icon-help-16-16.png' />",
+	"default_grid_header_help_label": "<img src='../images/icon-help-12-12.png' style='margin-left: -10px;' />",
 	"sample_string": "This is a default string, which will be used unless it is overridden by a language specific string."
  })
