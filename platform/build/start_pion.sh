7b9cf66f39188a0d9f5285e47ece11944172f039
Michael Dickey <mike@atomiclabs.com>
2011-04-15 19:30:07 +0000

    http://trac.atomiclabs.com/ticket/1123
    Added virtual memory limit to start_pion.sh script (Linux only)

--- platform/build/start_pion.sh
+++ platform/build/start_pion.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+#
+# start_pion.sh: Pion startup script for Unix tarball distributions
+
+# Limit pion's maximum virtual memory size by physical memory minus MEM_RESERVE
+# MEM_RESERVE is a percentage value from 0 through 100 (default = reserve 5%)
+MEM_RESERVE=5
+
+# Set global ulimits for all platforms
 ulimit -s 512
 ulimit -c unlimited
+
+# LINUX ONLY: Set maximum virtual memory
+if [ -r /proc/meminfo ]; then
+	ulimit -v $((`sed -n 's/^MemTotal: *\([0-9]*\).*$/\1/p' /proc/meminfo`*(100 - $MEM_RESERVE)/100))
+fi
+
 DYLD_LIBRARY_PATH="./libs" LD_LIBRARY_PATH="./libs" ./pion -c config/platform.xml $1
