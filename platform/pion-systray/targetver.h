d975f8f7f370219aa27e3121f2bd306b039d05bd
Vladimir Shcherbakov <vladimir@atomiclabs.com>
2011-09-08 04:23:55 +0000

    http://trac.atomiclabs.com/ticket/665 Forced XP as target (minimum) Windows version

--- platform/pion-systray/targetver.h
+++ platform/pion-systray/targetver.h
@@ -11,14 +11,8 @@
 #define WINVER _WIN32_WINNT_WINXP           // Change this to the appropriate value to target other versions of Windows.
 #endif
 
-#ifndef _WIN32_WINNT						// Specifies that the minimum required platform is Windows XP.
 #define _WIN32_WINNT _WIN32_WINNT_WINXP     // Change this to the appropriate value to target other versions of Windows.
-#endif
 
-#ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows 98.
-#define _WIN32_WINDOWS 0x0410	// Change this to the appropriate value to target Windows Me or later.
-#endif
+#define _WIN32_WINDOWS 0x0410
 
-#ifndef _WIN32_IE                       // Specifies that the minimum required platform is Internet Explorer 6.0.
 #define _WIN32_IE 0x0600        // Change this to the appropriate value to target other versions of IE.
-#endif
